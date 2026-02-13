fd8f6b7b7292146da498f7fedf5312c9c61e1051
Aung Phyo Thant <aungphyothant224@gmail.com>
2026-02-13 21:29:02 +0630

    - Update Socket Event.

--- chat-app-backend/src/modules/chat/chat.socket.ts
+++ chat-app-backend/src/modules/chat/chat.socket.ts
@@ -6,54 +6,55 @@ export const registerChatHandlers = (io: Server, socket: Socket) => {
   const { id, role, email } = socket.data.user;
   let admin: any = null;
 
-  socket.join(`user:${id}`);
-
   if (role === "ADMIN") {
     socket.join("admins");
     console.log(`Admin ${email} joined.`);
+    socket.on(
+      "admin_reply",
+      async (data: { receiverId: number; content: string }) => {
+        if (role !== "ADMIN") return;
+
+        if (!admin) {
+          admin = await getAdmin();
+        }
+
+        const msg = await saveMessage({
+          senderId: admin!.id,
+          receiverId: data.receiverId,
+          content: data.content,
+        });
+
+        await updateConversation({
+          userId: data.receiverId,
+          lastMessage: data.content,
+        });
+
+        io.to(`user:${data.receiverId}`).emit("receive_reply", msg);
+        io.to(`admins`).emit("new_user_message", msg);
+      },
+    );
   }
 
-  socket.on("message_to_admin", async (data: { content: string }) => {
-    if (!admin) {
-      admin = await getAdmin();
-    }
-
-    const msg = await saveMessage({
-      senderId: id,
-      receiverId: admin!.id,
-      content: data.content,
-    });
-
-    await updateConversation({
-      userId: id,
-      lastMessage: data.content,
-    });
-
-    io.to("admins").emit("new_user_message", msg);
-  });
-
-  socket.on(
-    "admin_reply",
-    async (data: { receiverId: number; content: string }) => {
-      if (role !== "ADMIN") return;
-
+  if (role === "USER") {
+    socket.join(`user:${id}`);
+    console.log(`User ${email} joined.`);
+    socket.on("message_to_admin", async (data: { content: string }) => {
       if (!admin) {
         admin = await getAdmin();
       }
 
       const msg = await saveMessage({
-        senderId: admin!.id,
-        receiverId: data.receiverId,
+        senderId: id,
+        receiverId: admin!.id,
         content: data.content,
       });
 
       await updateConversation({
-        userId: data.receiverId,
+        userId: id,
         lastMessage: data.content,
       });
 
-      io.to(`user:${data.receiverId}`).emit("receive_reply", msg);
-      io.to(`admins`).emit("new_user_message", msg);
-    },
-  );
+      io.to("admins").emit("new_user_message", msg);
+    });
+  }
 };
